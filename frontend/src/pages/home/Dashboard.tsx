ba497b86456d7a5a6cfa4e9079051c1fd4d75f1a
rabinabaga <rabinabaga@gmail.com>
2024-07-15 13:28:34 +0545

    sidebar toggle working in mobile

--- frontend/src/pages/home/Dashboard.tsx
+++ frontend/src/pages/home/Dashboard.tsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+
 const Dashboard = () => {
+const [showMenuMobile, setShowMenuMobile] = useState(false);
+  const btn = document.getElementById('mobile-menu-btn');
+  const menu = document.getElementById('mobile-menu');
+ const toggleMenu = ()=>{
+  setShowMenuMobile(!showMenuMobile)
+ }
+
+ 
   return (
     <div className="h-screen w-screen bg-gray-100 p-6">
       <div className="flex flex-col w-full h-full border border-primary-900">
         <div className="header p-4">
         <div className="flex justify-between">
+            <div className="block lg:hidden">
+              <button id="mobile-menu-btn" onClick={() => (setShowMenuMobile(!showMenuMobile))}className="text-white focus:outline-none">
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                </svg>
+              </button>
+            </div>
             <p className="text-2xl">WishMaker</p>
             <div className="flex w-[465px]">
                 <div className="search w-72">
@@ -14,10 +31,20 @@ const Dashboard = () => {
                 </div>
             </div>
         </div>
+          {/* <!-- Mobile menu --> */}
+          <div
+            className={`fixed relative lg:hidden bg-gray-800 text-white py-2 ${showMenuMobile ? 'w-full' : 'w-0'
+              }`}
+            style={{ transition: 'width 0.3s ease' }}
+          > 
+            <div className="absolute z-100 text-white-primary">Dashboard</div>
+            <div className="h-screen bg-opacity-50 bg-black-light" onClick={toggleMenu}></div>
+          </div>
+         
         </div>
         <div className="flex-1 border border-primary-900">
           <div className="flex">
-            <div className="sidebar w-80 p-5">
+            <div className={`sidebar hidden w-80 p-5 lg:block`}>
               <div className="w-full mb-4">
                 Dashboard
               </div>
